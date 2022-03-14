84b7c49ef6cefb75d8280f5e5f8e9cb85e108533
khalil <khalillakhdharatc@gmail.com>
2022-03-14 14:09:13 +0100

    Initial commit

---
+++ src/main/java/com/duranco/HibernateRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.duranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateRelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateRelationshipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duranco/HibernateRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duranco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateRelationshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
